b209e71a7cbf72dc12fef935050bc3a22b92c0f3
Adaobilynda1234 <adaobilynda1234@gmail.com>
2023-11-10 05:28:22 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -22,10 +22,18 @@ function App() {
       reminder: false,
     },
   ]);
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No task to show"
+      )}
     </div>
   );
 }
